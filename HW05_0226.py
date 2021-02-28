0b24f1d12a1a2378dfacf45f4169785aa713b086
朱瑞興 <jui.hsing.chu@gmail.com>
2021-02-28 18:32:37 +0800

    Create HW05_0226.py

---
+++ HW05_0226.py
@@ -0,0 +1,18 @@
+#HW05
+
+level = 9
+for i in range(level,0,-1):
+    for a in range(i,0,-1):
+        print (a, end='')
+    print()
+
+#for迴圈印出
+#123456789
+#12345678
+#1234567
+#123456
+#12345
+#1234
+#123
+#12
+#1
