11cffa46f9d447560d755a45ef1ea16d58b677a5
朱瑞興 <jui.hsing.chu@gmail.com>
2021-03-03 16:48:10 +0800

    Update HW04_0226.py

--- HW04_0226.py
+++ HW04_0226.py
@@ -1,16 +1,22 @@
 
 #【program】
-level = int(input("make triangle:"))
-for i in range(1,level):
-    for a in range(1,i+1):
-        print (a, end='')
+lv = int(input("make triangle:"))
+for i in range(lv):
+    for a in range(i+1):
+        print (a+1, end='')
     print()
 print("finish")
 
 #【result】
-#make triangle:5
+#make triangle:8
 #1
 #12
 #123
 #1234
+#12345
+#123456
+#1234567
+#12345678
+
+
 #finish
