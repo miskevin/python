b72d35039bf4cca88f019146f6be7cbfce52c706
朱瑞興 <jui.hsing.chu@gmail.com>
2021-03-08 19:04:07 +0800

    Update HW07_0226.py

--- HW07_0226.py
+++ HW07_0226.py
@@ -13,3 +13,13 @@ for height in range(-3,4,1):
     for b in range(2*(4-abs(height))-1):
         print("*",end='')
     print()
+
+    
+    
+    
+#teacher sol:
+for i in range(1,5):
+    print(' '*(4-i)+'*'*(2*i-1))
+    
+for i in range(3,0,-1):
+    print(' '*(4-i)+'*'*(2*i-1))
